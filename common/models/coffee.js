353046b1012aeff780b92abe351a867e4877f934
Gabriel <gaboelnuevo@gmail.com>
2017-07-30 09:46:27 -0600

    clarifai

--- common/models/coffee.js
+++ common/models/coffee.js
@@ -1,18 +1,38 @@
 'use strict';
 
+// instantiate a new Clarifai app passing in your api key.
+const clarifai = new Clarifai.App({
+ apiKey: proccess.env.CLARIFAI_KEY || ''
+});
+
+
 module.exports = function (Coffee) {
     Coffee.search = function (image, cb) {
-        Coffee.findOne({
-        }, function (err, docs) {
-            return cb(null, docs);
-        });
-
-        // return cb(null,"Hello");
+        // predict the contents of an image by passing in a url
+        clarifai.models.predict('Coffee', {
+            base64: image
+        }).then(response => {
+            var outputs = response.outputs && response.outputs.length >= 1 && response.outputs;
+            var data = outputs && outputs[0] && outputs[0].data;
+            var concepts = data && data.concepts.length >= 1 && data.concepts;
+            
+            if(concepts && concepts.length >= 1 && concepts[0].value >= 0.6){
+                Coffee.findOne({
+                    where: {
+                        brand: concepts[0].name
+                    }
+                }, function (err, docs) {
+                    cb(null, docs);
+                });
+            }else{
+                cb(null, null)
+            }
+        })
     };
 
     Coffee.remoteMethod("search", {
         description: "Searching coffee based on an image",
         accepts: { arg: "image", type: "string" },
-        returns: { arg: "result", type: "Object" }
+        returns: { arg: "result", type: "any", root: true}
     });
 };
