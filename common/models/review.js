4e2f35e78f86c1968132d969d241cdb9a1eb9a73
Gabriel <gaboelnuevo@gmail.com>
2017-09-04 14:16:42 -0600

    update review

--- common/models/review.js
+++ common/models/review.js
@@ -1,5 +1,36 @@
 'use strict';
 
-module.exports = function(Review) {
+var app = require('../../server/server');
 
+module.exports = function(Review) {
+    Review.observe('after save', function(ctx, next) {
+        var Coffee = app.models.Coffee;
+        var coffeeId = ctx.instance.coffeeId;
+        var collection = db.collection(Review.modelName);
+        var coffee = Review.getDataSource().ObjectID(coffeeId);
+        collection.aggregate([
+          { $match: { coffeId: coffee } },
+          { $group: {
+            _id: coffee,
+           rating: { $avg: "$rating" }
+          }}
+        ], function(err, data) {
+          if (err) {
+              next(err);
+          }else{
+            Coffee.findById(coffeeId)
+            .then(_coffee => {
+                if(data && data.rating){
+                    _coffee['avg_rating'] = data.rating;
+                }
+                return _coffee.save()
+                .then(_c => {
+                    next();
+                });
+            }).catch(_err => {
+                next(_err);
+            })
+          }
+        });
+    });
 };
